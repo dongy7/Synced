be4e156bc7e1c689b861a0304ce228913bce0c37
Dong Yeop Lee <dongy7@gmail.com>
2018-05-31 16:29:24 -0400

    add handler for seek in server

--- sockets/index.js
+++ sockets/index.js
@@ -4,24 +4,39 @@ module.exports = io => {
 
   io.on('connection', socket => {
     numClients++
-    // console.log('connected')
-    socket.on('pause', () => {
-      // console.log('client paused')
+    socket.on('pauseRequest', () => {
       io.emit('pause')
     })
 
     socket.on('playRequest', () => {
-      console.log('client requested play')
       io.emit('ready')
     })
 
     socket.on('ready', () => {
       readyCount++
-      console.log(`ready: ${readyCount}/${numClients}`)
       if (readyCount === numClients) {
         readyCount = 0
-        console.log('everyone ready to play')
         io.emit('play')
+      }
+    })
+
+    socket.on('seekReady', (data) => {
+      readyCount++
+      if (readyCount === numClients) {
+        readyCount = 0
+        io.emit('play')
+      }
+    })
+
+    socket.on('seekRequest', (data) => {
+      if (data.paused) {
+        io.emit('seek', {
+          time: data.time
+        })
+      } else {
+        io.emit('readySeek', {
+          time: data.time
+        })
       }
     })
 
